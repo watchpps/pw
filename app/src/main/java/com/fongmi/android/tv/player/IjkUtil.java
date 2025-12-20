f099a5af6973e1f2df9fb6d74240ba6b9e4aeac8
watchpps <82252878+watchpps@users.noreply.github.com>
2025-12-20 09:59:16 +0800

    Update IjkUtil.java

--- app/src/main/java/com/fongmi/android/tv/player/IjkUtil.java
+++ app/src/main/java/com/fongmi/android/tv/player/IjkUtil.java
@@ -1,32 +1,14 @@
 package com.fongmi.android.tv.player;
 
-import android.net.Uri;
-import com.fongmi.android.tv.Setting; // 修正后的路径
-import com.fongmi.android.tv.bean.Channel;
-import com.fongmi.android.tv.bean.Result;
-import com.fongmi.android.tv.utils.UrlUtil;
-import java.util.Map;
-import tv.danmaku.ijk.media.player.IjkMediaPlayer;
-import tv.danmaku.ijk.media.player.MediaSource;
+import com.fongmi.android.tv.Setting;
+import tv.danmaku.ijk.media.player.ui.IjkVideoView;
 import tv.danmaku.ijk.media.player.ui.IjkSubtitleView;
 
 public class IjkUtil {
 
-    public static MediaSource getSource(Result result) {
-        return getSource(result.getHeaders(), result.getRealUrl());
-    }
-
-    public static MediaSource getSource(Channel channel) {
-        return getSource(channel.getHeaders(), channel.getUrl());
-    }
-
-    public static MediaSource getSource(Map<String, String> headers, String url) {
-        Uri uri = UrlUtil.uri(url);
-        return new MediaSource(Players.checkUa(headers), uri);
-    }
-
-    public static void setSubtitle(IjkMediaPlayer ijk) {
-        if (ijk == null) return;
+    // 适配 Activity 中的调用：IjkUtil.setSubtitleView(mBinding.ijk)
+    public static void setSubtitleView(IjkVideoView ijk) {
+        if (ijk == null || ijk.getSubtitleView() == null) return;
         
         Object subtitleView = ijk.getSubtitleView();
         if (subtitleView instanceof IjkSubtitleView) {
@@ -34,6 +16,16 @@ public class IjkUtil {
             
             view.setApplyEmbeddedFontSizes(false);
             view.setApplyEmbeddedStyles(!Setting.isCaption());
+            
+            if (Setting.getSubtitleTextSize() != 0) {
+                view.setFractionalTextSize(Setting.getSubtitleTextSize());
+            }
+            if (Setting.getSubtitleBottomPadding() != 0) {
+                view.setBottomPaddingFraction(Setting.getSubtitleBottomPadding());
+            }
+        }
+    }
+}
             
             if (Setting.getSubtitleTextSize() != 0) {
                 view.setFractionalTextSize(Setting.getSubtitleTextSize());
